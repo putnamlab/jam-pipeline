e432680a8e61ed2d7d314c27bc78f578ed3f1c1e
Nicholas Putnam <nputnam@gmail.com>
2013-04-19 18:32:46 -0500

    make check_env.bash executable.

--- scripts/check_env.bash
+++ scripts/check_env.bash
@@ -1,6 +1,9 @@
-for f in kmerpipe.py fastq2pfa.pl # testmissing
+#!/usr/bin/env bash
+
+for f in kmerpipe.py fastq2pfa.pl gzcat gzip # testmissing
 do
     if [ `which $f` ] ; then echo "$f 	-- OK."; else echo "$f 	-- NOT FOUND."; fi
 done
 
 echo "If things weren't found, check if " $PWD"/"${0/check_env.bash/} " is in your PATH"
+
