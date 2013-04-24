e3b52a6ce3573e4b72c86188c54309b0c2bd1955
Paul Havlak <havlak@rice.edu>
2013-04-24 14:11:32 -0500

    Licensing.
    OligoTools for genome analysis by Paul Havlak is licensed under a
    Creative Commons Attribution 3.0 Unported License.

---
+++ src/OligoTools.hh
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////
+// OligoTools
+// = OligoTools for genome analysis
+// by Paul Havlak
+// copyright 2009-2013 Rice University
+//
+// OligoTools for genome analysis by Paul Havlak is licensed under a 
+// <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.en_US">
+// Creative Commons Attribution 3.0 Unported License.
+// </a>
+//
+// OligoTools for genome analysis incorporates unpublished modules for Kmer 
+// hash tables originally developed under the Atlas Project 
+// (Atlas Whole Genome Assembly Suite) at Baylor College of Medicine
+// Human Genome Sequencing Center during 2003-2006.
+// The BCM-HGSC copyrighted predecessor is available through the 
+// Atlas WGA Suite source distribution on the HGSC web site:
+//    http://www.hgsc.bcm.tmc.edu/content/atlas-whole-genome-assembly-suite
+// 
+// Extensive modifications and additional modules, including all features
+// for SNPmer pairing and library bitvectors, were developed in the Putnam
+// Lab at Rice University:
+//    http://nputnam.web.rice.edu
+// 
+// Open-source repository: part of the Putnam Lab JAM project
+//    https://github.com/putnamlab/jam-pipeline
+//    https://github.com/putnamlab/jam-pipeline/tree/master/source
+//     
+// Contact: Paul Havlak:
+//    havlak@rice.edu
+//    havlak@alumni.rice.edu
+//    http://www.linkedin.com/in/havlak
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef OLIGOTOOLS_HH
+#define OLIGOTOOLS_HH 1
+string OligoToolsCredits = 
+  "Part of the Putnam Lab JAM project, copyright 2009-2013 by Nik Putnam and Paul Havlak, Rice University.\n" 
+  "  https://github.com/putnamlab/jam-pipeline\n"
+  "Licensed under the Creative Commons Attribution 3.0 Unported License. To view a copy of this license, visit\n"
+  "  http://creativecommons.org/licenses/by/3.0\n";
+  "Employs OligoTools, initially developed at BCM-HGSC as a successor to the Atlas Kmer libraries:\n"
+  "  http://www.hgsc.bcm.tmc.edu/content/atlas-whole-genome-assembly-suite\n"
+  "      SimpleMerHash.{cc,hh}\n"
+#endif
